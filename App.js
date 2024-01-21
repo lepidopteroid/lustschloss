939aeace5ac74dbdb11038f6a28310cffd3c8509
Ephor <40041619+lepidopteroid@users.noreply.github.com>
2024-01-21 16:46:31 +0000

    add three.js and expo-three

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GLView } from 'expo-gl'; 
 
 export default function App() {
   return (
